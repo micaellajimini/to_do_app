d5639b7119118f1f8ef81dec3ef1e179812b3094
micaellajimini <jimin7971@gmail.com>
2018-11-23 15:52:26 +0900

    just simple things

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , StatusBar, TextInput, Dimensions, Platform } from 'react-native';
 
+const {height,width} = Dimensions.get("window");
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do List</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"}/>
+        </View>
       </View>
     );
   }
@@ -14,8 +19,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize : 30,
+    fontWeight : "500",
+    marginTop : 50,
+    marginBottom: 30,
+  },
+  card : {
+    backgroundColor : "white",
+    flex : 1,
+    width : width - 25,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    ...Platform.select({
+      ios : {},
+      android : {
+        elevation : 3,
+      },
+    })
+  }
 });
