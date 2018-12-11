7dba52465555c43c09db2a6f49e78d0c1ce15e28
micaellajimini <jimin7971@gmail.com>
2018-12-12 00:34:28 +0900

    state change example

--- todo.js
+++ todo.js
@@ -5,18 +5,26 @@ const {width, height} = Dimensions.get("window");
 export default class Todo extends React.Component{
   state = {
     isEdit : false,
+    isCompleted : false,
   }
   render(){
+    const {isCompleted } = this.state;
     return(
       <View style={styles.container2}>
-        <TouchableOpacity>
-          <View style={styles.circle}>
-
-          </View>
-          <Text style={styles.textstyle}>Text123</Text>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
         </TouchableOpacity>
+        <Text style={styles.textstyle}>Text123</Text>
       </View>
     );
+  }
+
+  _onPressButton = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted : !prevState.isCompleted
+      }
+    })
   }
 }
 
@@ -26,9 +34,21 @@ const styles = StyleSheet.create({
     borderBottomColor : "#bbb",
     borderBottomWidth : StyleSheet.hairlineWidth,
     flexDirection : "row",
+    alignItems : "center",
   },
   circle : {
-
+    width: 30,
+    height: 30,
+    borderRadius : 15,
+    backgroundColor : "white",
+    borderWidth: 3,
+    marginRight : 20,
+  },
+  completedCircle : {
+    borderColor : "#bbb",
+  },
+  uncompletedCircle : {
+    borderColor : "#F23657",
   },
   textstyle : {
     fontWeight : "600",
